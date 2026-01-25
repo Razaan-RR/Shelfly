ae9b7766d7530ff9ddfa5f00126a6be833fded40
Razaan <razaanreza0705@gmail.com>
2026-01-25 18:04:30 +0600

    updated env

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,16 +2,14 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxGTMxmK6s7rxLJcs0ge5KT9K3hPuUqi8",
-  authDomain: "shelfly-auth.firebaseapp.com",
-  projectId: "shelfly-auth",
-  storageBucket: "shelfly-auth.firebasestorage.app",
-  messagingSenderId: "772903468541",
-  appId: "1:772903468541:web:3cdd882ea6e4c36ff02d94",
-  measurementId: "G-747YKKFTJD"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
-
-// THIS is what you were missing
 export const auth = getAuth(app)
